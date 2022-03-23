1f1eaa4812386caf9d49df6def0ec76897649fcd
Ed Nava <eduardonava500@gmail.com>
2022-03-23 18:05:21 -0400

    Reformmated file and added dummy firebase logic to check passwords

--- src/screens/auth/ResetPassword/index.js
+++ src/screens/auth/ResetPassword/index.js
@@ -1,71 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, 
-  TouchableWithoutFeedback, Keyboard, TextInput} from 'react-native';
+import {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet, Text, View, KeyboardAvoidingView, Platform,
+  TouchableWithoutFeedback, Keyboard, TextInput
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Button } from '@ant-design/react-native';
 
 const Reset_Password = () => {
-    return (
-      <View style={styles.container}>
-      <View style={styles.title}>
-      <Text style={{ fontSize: 27, letterSpacing: 0, opacity: 1 }}>
-      Reset Password
-        </Text>
-      </View>
-         <KeyboardAvoidingView
-         behavior={Platform.OS === "ios" ? "padding" : "height"}
-         style={styles.container}
-       >
-         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-           <View style={styles.inner}>
-             <TextInput placeholder="New Password" style={styles.textInput} secureTextEntry={true} />
-           </View>
-         </TouchableWithoutFeedback>
-       </KeyboardAvoidingView>
-       <View style={styles.buttons}>
-       <Button type="primary" style={{ flex : .88, backgroundColor: "#4053FA" }}>
-          <Text style={{ fontSize: 18, fontWeight: "600" }}>
-            Reset
-          </Text>
-        </Button>
-         </View>
-      </View>
-    )
+
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  function talkToFirebase(p, cp) {
+    if (p == cp) {
+      console.log(p);
+    }
+    else {
+      alert("Passwords do not match!")
+    }
+  }
+
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container}>
+      <SafeAreaProvider>
+        <StatusBar />
+        <View style={{ flex: 0.8, alignContent: 'center', justifyContent: 'flex-end' }}>
+          <View style={{ flex: 0.2, flexDirection: 'row', alignContent: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 24 }}>
+              Reset Password
+            </Text>
+          </View>
+          <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="New Password" secureTextEntry={true} />
+          <TextInput style={styles.input} onChangeText={setConfirmPassword} value={confirmPassword} placeholder="Confirm Password" secureTextEntry={true} />
+        </View>
+        <View style={{ flex: 0.6, alignContent: 'center' }}>
+          <Button type="primary" style={{ height: 40, margin: 12, borderWidth: 1, padding: 10, backgroundColor: "#4053FA" }} onPress={() => talkToFirebase(password, confirmPassword)}>
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>
+              Reset
+            </Text>
+          </Button>
+        </View>
+      </SafeAreaProvider>
+    </KeyboardAvoidingView >
+  )
 }
 
 const styles = StyleSheet.create({
-   
-    title: {
-      flex: 0.2,
-      flexDirection: "column",
-      justifyContent: "center",
-      left: "10%",
-      marginTop: "10%",
-    },
-    buttons: {
-      flex: 0.2,
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-evenly",
-    },
-    container: {
-      flex: 1
-    },
-    inner: {
-      padding: 24,
-      flex: 1
-    },
-    textInput: {
-      height: 40,
-      borderColor: "#000000",
-      borderBottomWidth: 1,
-      marginBottom: 36
-    },
-    btnContainer: {
-      backgroundColor: "white",
-      marginTop: 12
-    }
-  });
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    backgroundColor: "#fff"
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+});
 
-  
-  export default Reset_Password
-  
+
+export default Reset_Password
