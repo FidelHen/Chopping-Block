cbb9bd44fe4e470b0fba2c81835c6754e66f3d35
Ed Nava <eduardonava500@gmail.com>
2022-03-23 15:56:46 -0400

    Added temp Main user screen after login

---
+++ src/screens/main/Main/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+const Main = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <View style={{flexDirection: "row", justifyContent: "center"}}>
+        <Text>
+          Main Screen
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "#fff"
+  },
+});
+
+export default Main;
