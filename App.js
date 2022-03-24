18e21ccd5126f042f10b94b400e942d94638bd68
Ed Nava <eduardonava500@gmail.com>
2022-03-24 19:30:17 -0400

    Changed initialRoute to Landing and added Cavings page

--- App.js
+++ App.js
@@ -4,19 +4,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Landing from './src/screens/auth/Landing';
 import SignUp from './src/screens/auth/SignUp';
 import Login from './src/screens/auth/Login';
-import Home from './src/screens/main/Home';
 import ResetPassword from './src/screens/auth/ResetPassword';
+import Cravings from './src/screens/main/Cravings';
+import Home from './src/screens/main/Home';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName="Landing" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="ResetPassword" component={ResetPassword} />
+        <Stack.Screen name="Cravings" component={Cravings} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
