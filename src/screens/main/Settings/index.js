400bdd7adfb39a8d01206afa9814f1911068e715
Ed Nava <eduardonava500@gmail.com>
2022-03-31 17:12:52 -0400

    Fixed settings page up a bit

--- src/screens/main/Settings/index.js
+++ src/screens/main/Settings/index.js
@@ -1,66 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ProfilePic from '../../../assets/svgs/profilePicSVG';
+import { auth } from '../../../firebase/firebase';
 
 const Settings = ({ navigation }) => {
+
+  useEffect(() => {
+    console.log(auth)
+  }, [auth])
+  
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaProvider>
         <StatusBar />
+
+        <View style={{ flex: 0.15, marginLeft: '5%', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <AntDesign name='left' size={28} color="#000000" />
+          </TouchableOpacity>
+        </View>
+
         <View style={styles.title}>
-          <Text style={{ fontSize: 40, letterSpacing: 0, opacity: 1 }}>
+          <Text style={{ fontSize: 32, letterSpacing: 0, opacity: 1 }}>
             Settings
           </Text>
         </View>
 
         <View style={styles.image_container}>
-          <View style={{flex: 0.4}}>
+          <View style={{ flex: 0.7 }}>
             <ProfilePic />
           </View>
-          <View style={{flex: 0.2}}>
+          <View style={{ flex: 0.2 }}>
             <Text style={{ fontSize: 25, letterSpacing: 0, opacity: 1 }}>
-              Peter Glover
+              {auth.currentUser?.displayName}
             </Text>
           </View>
-          <View>
+          <View style={{ flex: 0.4 }}>
             <Text>
-              peter456@gmail.com
+              {auth.currentUser?.email}
             </Text>
           </View>
         </View>
 
         <View style={styles.image_text}>
-          <Text style={{
-            fontSize: 27, fontWeight: "bold", letterSpacing: 0, opacity: 1
-          }}>
-            Account Setting
-          </Text>
-
-          <Text style={{
-            fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1
-          }}>
-            Edit Name:
-          </Text>
-
-          <Text style={{
-            fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1
-          }}>
-            Recent Groups:
-          </Text>
-
-          <Text style={{
-            fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1
-          }}>
-            Favorites:
-          </Text>
-
-          <Text style={{
-            fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1
-          }}>
-            Reset Password:
-          </Text>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 27, fontWeight: "bold", letterSpacing: 0, opacity: 1 }}>
+              Account Setting
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1 }}>
+              Edit Name
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1 }}>
+              Recent Groups
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1 }}>
+              Favorites
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1 }}>
+              Reset Password
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Landing")}>
+            <Text style={{ fontSize: 20, textAlign: "center", letterSpacing: 0, opacity: 1 }}>
+              Log Out
+            </Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaProvider>
     </SafeAreaView >
@@ -69,24 +84,22 @@ const Settings = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     flexDirection: "column",
     justifyContent: "space-evenly",
     backgroundColor: "#fff"
   },
   title: {
-    flex: 0.2,
+    flex: 0.1,
     flexDirection: "row",
     justifyContent: "center",
-    marginTop: "2%",
-    alignContent: "center"
+    alignContent: "center",
   },
   image_container: {
     flex: 0.4,
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    padding: "5%"
   },
   image_text: {
     flex: 0.8,
@@ -94,14 +107,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     marginLeft: "5%",
     alignItems: "flex-start",
-  },
-  small_circles: {
-    flex: 0.1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginLeft: "44%",
-    marginRight: "44%",
   },
   buttons: {
     flex: 0.2,
