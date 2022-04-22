a73931b725de9c5c6a77b11b9cd49618e4b69bbe
Ed Nava <eduardonava500@gmail.com>
2022-04-22 18:46:26 -0400

    Added user preferences to database

--- src/screens/main/Cravings/index.js
+++ src/screens/main/Cravings/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, View } from 'react-native';
 import { Button } from '@ant-design/react-native';
+import { auth } from '../../../firebase/firebase';
+import { db } from '../../../firebase/firebase';
+import { doc, setDoc } from 'firebase/firestore';
 
 const Cravings = ({ navigation }) => {
+
+  const [perf, setPerf] = useState([]);
+
+  async function addCravingsToUser(perf) {
+    const docRef = doc(db, 'users', auth.currentUser?.uid);
+    await setDoc(docRef, { 
+      perferences: perf 
+    }, { merge: true }).then(() => {
+      alert("Added Preferences Successfully")
+    }).catch(error => {
+      alert("Error adding preferences");
+      alert(error.messages);
+    });
+  }
+
   return (
     <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-evenly", }}>
       <View style={styles.title}>
@@ -20,10 +38,15 @@ const Cravings = ({ navigation }) => {
               <Text style={{ fontSize: 15, letterSpacing: 0, opacity: 1 }}>
                 Prefrences
               </Text>
-              <TextInput placeholder="Ex. Vegan, Korean, Mexican" />
+              <TextInput onChangeText={setPerf} value={perf} placeholder="Ex. Vegan, Korean, Mexican" />
             </View>
             <View style={styles.buttons}>
-              <Button type="primary" style={{ flex: 0.75, backgroundColor: "#4053FA" }} onPress={() => navigation.navigate("Home")} >
+              <Button type="primary" style={{ flex: 0.75, backgroundColor: "#4053FA" }} onPress={() => {
+                const perfArray = perf.split(",").map((value) => value.trim())
+                console.log(perfArray)
+                addCravingsToUser(perfArray)
+                navigation.navigate("Home")
+              }} >
                 <Text style={{ fontSize: 18, fontWeight: "600" }}>
                   Next
                 </Text>
